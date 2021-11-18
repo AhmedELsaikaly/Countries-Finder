9d14e8e970bd87c3bab02cfaf3b2fbb078d06354
Ahmed ELsaikaly <aelsaikaly@itgsoftware.com>
2021-11-18 15:25:49 +0200

    add files

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,8 +1,12 @@
+import { useEffect } from "react";
 import { Result } from "antd";
 
 import classes from "./styles.module.scss";
 
 const NotFound = () => {
+  useEffect(() => {
+    document.title = "Not Found";
+  }, []);
   return (
     <Result
       className={classes.notFound}
